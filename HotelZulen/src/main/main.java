60557fed108d9ef439b780ddc3a7e5881f75daf9
pochoclos <andre.melendezc@unmsm.edu.pe>
2024-09-15 01:23:02 -0500

    crud_personal

--- HotelZulen/src/main/main.java
+++ HotelZulen/src/main/main.java
@@ -25,11 +25,14 @@ public class main {
         boolean flag = true;
 
         do {
+            System.out.println("------------------");
             System.out.println("Menu de opciones");
+            System.out.println("------------------");
             System.out.println("1. CRUD personal");
             System.out.println("2. CRUD huesped");
             System.out.println("3. CRUD habitacion");
             System.out.println("4. Salir");
+            System.out.println("------------------");
             System.out.println("Que desea hacer?");
             int op = sc.nextInt();
             sc.nextLine(); //Limpiar el buffer
@@ -55,31 +58,66 @@ public class main {
         PersonalCrud personalCrud = new PersonalCrud();
         boolean flagPersonal = true;
         do {
+            System.out.println("\n------------------");
+            System.out.println("CRUD PERSONAL");
+            System.out.println("------------------");
             System.out.println("1. Agregar personal");
             System.out.println("2. Leer todo personal");
-            System.out.println("3. Actualizar personal");
-            System.out.println("4. Eliminar personal");
-            System.out.println("5. Retroceder");
+            System.out.println("3. Buscar personal");
+            System.out.println("4. Actualizar personal");
+            System.out.println("5. Eliminar personal");
+            System.out.println("6. Retroceder");
+            System.out.println("------------------");
             System.out.println("Que desea hacer?");
             int opPersonal = sc.nextInt();
             sc.nextLine();
 
             switch (opPersonal) {
                 case 1:
+                    System.out.println("\n-----------------------------");
+                    System.out.println("Agregando personal");
+                    System.out.println("-----------------------------");
                     System.out.println("Nombre: ");
                     String nombre = sc.nextLine();
                     System.out.println("Apellido: ");
                     String apellido = sc.nextLine();
-                    Personal nuevoPersonal = new Personal(nombre,apellido);
+                    Personal nuevoPersonal = new Personal(nombre, apellido);
                     personalCrud.agregarPersonal(nuevoPersonal);
+                    System.out.println("-----------------------------\n");
                     break;
                 case 2:
+                    System.out.println("\n-----------------------------");
+                    System.out.println("Imprimiendo todo el personal");
+                    System.out.println("-----------------------------\n");
+                    personalCrud.leerTodoPersonal();
                     break;
                 case 3:
+                    System.out.println("\n-----------------------------");
+                    System.out.println("Buscando personal");
+                    System.out.println("-----------------------------");
+                    System.out.println("ID a buscar: ");
+                    int id_buscar = sc.nextInt();
+                    personalCrud.buscarPersonal(id_buscar);
+                    System.out.println("-----------------------------\n");
                     break;
                 case 4:
+                    System.out.println("\n-----------------------------");
+                    System.out.println("Actualizando personal");
+                    System.out.println("-----------------------------");
+                    System.out.println("ID a actualizar: ");
+                    int id_actualizar = sc.nextInt();
+                    personalCrud.actualizarPersonal(id_actualizar);
+                    System.out.println("-----------------------------\n");
                     break;
                 case 5:
+                    System.out.println("\n-----------------------------");
+                    System.out.println("Eliminando personal");
+                    System.out.println("-----------------------------");
+                    System.out.println("ID a eliminar: ");
+                    int id_eliminar = sc.nextInt();
+                    personalCrud.eliminarPersonal(id_eliminar);
+                    System.out.println("-----------------------------\n");
+                case 6:
                     flagPersonal = false;
                     break;
             }
