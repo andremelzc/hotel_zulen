fb930a35a1929df119ea20c0c7131a50674fb770
fabrizio Mantari <fabrizio.mantari@unmsm.edu.pe>
2024-09-16 21:45:23 -0500

    giron

---
+++ HotelZulen/src/Clases_Fabri/Housekeeper.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases_Fabri;
+
+/**
+ *
+ * @author Fabrizio Mantari
+ */
+public class Housekeeper extends Personal{
+
+    public Housekeeper(String Nombre, String Apellido, String DNI, String telefono, String dirección, String ID, String usuario, String contraseña, String funcion) {
+        super(Nombre, Apellido, DNI, telefono, dirección, ID, usuario, contraseña, funcion);
+    }
+    
+    public void habitacionesAsignadas(){
+        
+    } 
+}
---
+++ HotelZulen/src/Clases_Fabri/Reservación.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases_Fabri;
+
+import java.util.List;
+
+/**
+ *
+ * @author Fabrizio Mantari
+ */
+public class Reservación {
+    private String cliente;
+    private List servicios;
+}
