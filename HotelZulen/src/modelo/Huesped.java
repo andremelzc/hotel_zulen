de0b074a604d64ee398d26ca0d73760882382f7f
pochoclos <andre.melendezc@unmsm.edu.pe>
2024-09-29 21:24:58 -0500

    vvvvvvvvvvv

--- HotelZulen/src/modelo/Huesped.java
+++ HotelZulen/src/modelo/Huesped.java
@@ -1,8 +1,5 @@
 package modelo;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class Huesped {
     private int ID;
     private String nombre;
@@ -30,25 +27,6 @@ public class Huesped {
        
    }
    
-    public Huesped(int ID, String nombre, String apellido, int DNI, int telefono, String direccion, String usuario, String contrasena) {
-        this.ID = ID;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.DNI = DNI;
-        this.telefono = telefono;
-        this.direccion = direccion;
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-    }
-
-    public Huesped(String nombre, String apellido, String usuario, String contrasena, int IDHabitacion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-
-    }
-
     public String getNombre() {
         return nombre;
     }
