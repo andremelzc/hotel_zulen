46bd2d49cf075477d4975965e816be7851fe2d2d
Suyco <pedro.suycp@unmsm.edu.pe>
2024-11-02 05:48:50 -0500

    conectando a BD v1.0

---
+++ HotelZulen/src/Persistencia/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package Persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Suyco
+ */
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hotel_zulen";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Pablopalero1?";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    
+    // Método para probar la conexión
+    public static void testConnection() {
+        try (Connection connection = getConnection()) {
+            System.out.println("Conexion exitosa a la base de datos!");
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+
+    }
+}
---
+++ HotelZulen/src/interfaz/BD_Pruebas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaz;
+
+import Persistencia.DatabaseConnection;
+
+/**
+ *
+ * @author Suyco
+ */
+public class BD_Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DatabaseConnection.testConnection();
+    }
+    
+}
