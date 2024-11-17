2ad4f346ce55f639528ce51d1d2115316bda02c3
fabrizio Mantari <fabrizio.mantari@unmsm.edu.pe>
2024-11-17 15:03:07 -0500

    funcion asignacion housekeeper 1.0

--- HotelZulen/src/Persistencia/LimpiezaRepository.java
+++ HotelZulen/src/Persistencia/LimpiezaRepository.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -18,11 +19,24 @@ import modelo.Limpieza;
  *
  * @author Fabrizio Mantari
  */
-public class LimpiezaRepository implements IRepository<Limpieza>{
+public class LimpiezaRepository implements IRepository<Limpieza> {
 
     @Override
     public void crear(Limpieza objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String query = "INSERT INTO hotel_zulen.Limpiezas (HABITACIONES_idHabitaciones, HABITACIONES_TIPO_HAB_idCategoria, PERSONAL_DNI, FechaLimpieza, TipoLimpieza, estadoLimpieza) VALUES (?,?,?,?,?,?)";
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, objeto.getIdHabitacion());      // idHabitacion
+            stmt.setInt(2, objeto.getCategoriaHab());     // categoria
+            stmt.setInt(3, objeto.getPersonalDNI());      // personalDNI
+            stmt.setDate(4, java.sql.Date.valueOf(objeto.getFechaLimpieza())); // fechaLimpieza
+            stmt.setString(5, objeto.getTipoLimpieza());  // tipoLimpieza
+            stmt.setString(6, objeto.getEstadoLimpieza());// estadoLimpieza
+            stmt.executeUpdate();
+            System.out.println("Limpieza asignada con exito");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -39,41 +53,69 @@ public class LimpiezaRepository implements IRepository<Limpieza>{
     public void eliminar(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
-    public void asignarLimpiezas(){
-        String query1 = "SELECT " +
-               "idHabitaciones, " +
-               "Estado, " +
-               "CASE " +
-               "    WHEN Estado = 'CheckOut' THEN 4 " +
-               "    WHEN Estado = 'Ocupado' THEN 3 " +
-               "    WHEN Estado IN ('Disponible', 'Reservado') THEN 2 " +
-               "    ELSE 1 " +
-               "END AS peso " +
-               "FROM habitaciones " +
-               "ORDER BY peso, Estado, idHabitaciones;";
-        
+
+    public void asignarLimpiezas() {
+        String query1 = "SELECT "
+                + "idHabitaciones, "
+                + "TIPO_HAB_idCategoria, "
+                + "Estado, "
+                + "CASE "
+                + "    WHEN Estado = 'CheckOut' THEN 4 "
+                + "    WHEN Estado = 'Ocupado' THEN 3 "
+                + "    WHEN Estado IN ('Disponible', 'Reservado') THEN 2 "
+                + "    ELSE 1 "
+                + "END AS peso "
+                + "FROM habitaciones "
+                + "ORDER BY peso, Estado, idHabitaciones;";
+
         List<int[]> habitacionesConPesos = new ArrayList<>();
-        try(Connection connection = DatabaseConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(query1)){
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(query1)) {
             ResultSet resultSet = stmt.executeQuery();
-            while(resultSet.next()){
-                int fila [] = new int[2];
+            while (resultSet.next()) {
+                int fila[] = new int[3];
                 fila[0] = resultSet.getInt("idHabitaciones");
                 fila[1] = resultSet.getInt("peso");
+                fila[2] = resultSet.getInt("TIPO_HAB_idCategoria");  // Añadir la categoría
                 habitacionesConPesos.add(fila);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
+        }
+
+        String query2 = "SELECT DNI FROM personal WHERE TipoPersonal = 'Housekeeper' AND Estado = 'Activo';";
+        PriorityQueue<int[]> housekeepers = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0])); // Priority queue de los housekeeper (PESO,DNI)
+
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(query2)) {
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                int DNI = resultSet.getInt("DNI");
+                housekeepers.add(new int[]{0, DNI});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        List<Limpieza> nuevosRegistros = new ArrayList<>();
+
+        for (int[] fila : habitacionesConPesos) {
+
+            LocalDate fechaHoy = LocalDate.now();
+
+            int[] aux = housekeepers.poll();
+
+            int idHabitacion = fila[0];
+            int peso = fila[1];
+            int idCategoria = fila[2];  // Recuperamos la categoría
+
+            nuevosRegistros.add(new Limpieza(idHabitacion, aux[1], idCategoria, "Profunda", "asignada", fechaHoy));
+
+            aux[0] += peso; //sumando el peso de la habitacion al peso acumulado del housekeeper                     
+
+            housekeepers.add(aux);
         }
         
-        String query2 = "SELECT DNI FROM personal WHERE TipoPersonal = 'Housekeeper' AND Estado = 'Activo';";
-        try(Connection connection = DatabaseConnection.getConnection(); PreparedStatement stmt = connection.prepareStatement(query2)){
-            ResultSet resultSet = stmt.executeQuery();
-            PriorityQueue<int[]> queue = new PriorityQueue<>();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-            
+        for(Limpieza registro : nuevosRegistros){
+            crear(registro);
         }
     }
 }
--- HotelZulen/src/modelo/Limpieza.java
+++ HotelZulen/src/modelo/Limpieza.java
@@ -4,10 +4,78 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Fabrizio Mantari
  */
 public class Limpieza {
+    private int idHabitacion;
+    private int personalDNI;
+    private int categoriaHab;
+    private String tipoLimpieza;
+    private String estadoLimpieza;
+    private LocalDate fechaLimpieza;
+
+    public Limpieza(int idHabitacion, int personalDNI, int categoriaHab, String tipoLimpieza, String estadoLimpieza, LocalDate fechaLimpieza) {
+        this.idHabitacion = idHabitacion;
+        this.personalDNI = personalDNI;
+        this.categoriaHab = categoriaHab;
+        this.tipoLimpieza = tipoLimpieza;
+        this.estadoLimpieza = estadoLimpieza;
+        this.fechaLimpieza = fechaLimpieza;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public int getPersonalDNI() {
+        return personalDNI;
+    }
+
+    public int getCategoriaHab() {
+        return categoriaHab;
+    }
+
+    public String getTipoLimpieza() {
+        return tipoLimpieza;
+    }
+
+    public String getEstadoLimpieza() {
+        return estadoLimpieza;
+    }
+
+    public LocalDate getFechaLimpieza() {
+        return fechaLimpieza;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public void setPersonalDNI(int personalDNI) {
+        this.personalDNI = personalDNI;
+    }
+
+    public void setCategoriaHab(int categoriaHab) {
+        this.categoriaHab = categoriaHab;
+    }
+
+    public void setTipoLimpieza(String tipoLimpieza) {
+        this.tipoLimpieza = tipoLimpieza;
+    }
+
+    public void setEstadoLimpieza(String estadoLimpieza) {
+        this.estadoLimpieza = estadoLimpieza;
+    }
+
+    public void setFechaLimpieza(LocalDate fechaLimpieza) {
+        this.fechaLimpieza = fechaLimpieza;
+    }
+    
+    
     
 }
